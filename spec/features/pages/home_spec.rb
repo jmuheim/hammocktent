ca91a00b36f94d9f6a233ee071d260a4a347b5d1
Joshua Muheim <josh@josh.ch>
2014-11-25 17:32:27 +0100

    Fix specs

--- spec/features/pages/home_spec.rb
+++ spec/features/pages/home_spec.rb
@@ -4,6 +4,6 @@ describe 'Showing the home page' do
   it 'displays a welcome message' do
     visit root_path
 
-    expect(page).to have_content 'Welcome to Hammock Tent!'
+    expect(page).to have_content 'The 90 Degree Hammock – possibly the most comfortable and versatile hammock in the world.'
   end
 end
