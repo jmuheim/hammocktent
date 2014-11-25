158b5a17e5c8b7327a130b090e0b6fd7bada7ea1
Joshua Muheim <josh@josh.ch>
2014-11-25 12:00:54 +0100

    Fix some stuff

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -2,7 +2,7 @@ module VideoHelper
   def video(url)
     content_tag :p, class: 'movie' do
       # TODO: Open in fancybox!
-      link_to 'https://www.youtube.com/watch?v=L4VamzsV3Uk', target: '_blank' do
+      link_to url, target: '_blank' do
         icon(:film) + '&nbsp;Watch the video'.html_safe
       end
     end
