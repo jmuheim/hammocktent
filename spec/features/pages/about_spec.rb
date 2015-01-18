b835e07ef2aec9dade33ce2a59b93014204f816a
Joshua Muheim <josh@josh.ch>
2015-01-18 15:20:37 +0100

    Remove about spec (we don't have an about page anymore)

--- spec/features/pages/about_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'Showing about page' do
-  it 'displays a welcome message' do
-    visit page_path('about')
-
-    expect(page).to have_content 'About Hammock Tent'
-  end
-end
