8ca9e8429a8fc39c920e1806f747c2904dc43652
Joshua Muheim <josh@josh.ch>
2015-01-23 13:05:53 +0100

    More translations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,6 +4,6 @@ module ProductsHelper
   end
 
   def price(amount, additional_info = nil)
-    "Price #{additional_info} (CHF): <span class='price'>#{format('%.2f', amount)}.-</span> <sup>excl. <a class='vat' title='For Swiss customers only'>VAT*</a> and shipping</sup>".html_safe
+    "#{t 'helpers.products.price'} #{additional_info} (CHF): <span class='price'>#{format('%.2f', amount)}.-</span> <sup>excl. <a class='vat' title='For Swiss customers only'>VAT*</a> and shipping</sup>".html_safe
   end
 end
--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -3,7 +3,7 @@ module VideoHelper
     content_tag :p, class: 'movie' do
       # TODO: Open in fancybox!
       link_to url, target: '_blank' do
-        icon(:film) + '&nbsp;Watch the video'.html_safe
+        icon(:film) + t('helpers.video.watch_the_video').html_safe # TODO: Add i18n "helpers" namespace so we can use t('.video.watch_the_video')
       end
     end
   end
