0bf0ee806410f2e449b70d987da0f29249b61da2
Joshua Muheim <josh@muheimwebdesign.ch>
2015-05-01 19:53:43 +0200

    Add translations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,6 +4,7 @@ module ProductsHelper
   end
 
   def price(amount, additional_info = nil)
-    "#{t 'helpers.products.price'} #{additional_info} (CHF): <span class='price'>#{format('%.2f', amount)}.-</span> <sup>excl. <a class='vat' title='For Swiss customers only'>VAT*</a> and shipping</sup>".html_safe
+    "#{t 'helpers.products.price'} #{additional_info} (CHF): <span class='price'>#{format('%.2f', amount)}.-</span>
+    <sup>#{t 'helpers.products.excl_vat_and_shipping', vat: link_to(t('helpers.products.vat'), nil, class: 'vat', title: t('helpers.products.for_swiss_customers_only'))}</sup>".html_safe
   end
 end
