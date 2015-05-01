28b904eaaa7231b0f33f898a7075689341afc5bd
Joshua Muheim <josh@muheimwebdesign.ch>
2015-05-01 19:40:44 +0200

    Add videos with german subtitles for german page

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,46 @@
+module PagesHelper
+  def how_to_hang_and_pack_up_the_hammock_video
+    case I18n.locale
+    when :de
+      'J-y4TsYxBKc'
+    else # Use english video by default
+      'XrLM8wUJpGk'
+    end
+  end
+
+  def hammock_tips_and_tricks_video
+    case I18n.locale
+    when :de
+      'zPrGS2eDbj8'
+    else # Use english video by default
+      'S0lHjw4HskM'
+    end
+  end
+
+  def how_to_pitch_the_tarp_tent_above_the_hammock_video
+    case I18n.locale
+    when :de
+      'I1rEXSW1ZSA'
+    else # Use english video by default
+      'udTGsfq7qe0'
+    end
+  end
+
+  def how_to_pitch_the_tarp_tent_on_the_ground_video
+    case I18n.locale
+    when :de
+      'UQo0RUnKUTM'
+    else # Use english video by default
+      'D0a67tRsxm8'
+    end
+  end
+
+  def tipps_and_tricks_how_to_keep_the_guy_lines_tidy_video
+    case I18n.locale
+    when :de
+      'Im39UA1pyhs'
+    else # Use english video by default
+      'vJChN2CPmZs'
+    end
+  end
+end
